6047cb57025691ffea8034e8eb6ae511f4771e00
Gaston Gutierrez Llomplat <gastong7337@gmail.com>
2025-04-15 15:45:29 -0500

    Carga clientes

--- tp4_2.c
+++ tp4_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_CLIENTES 5
 #define MAX_PRODUCTOS 10
@@ -11,18 +12,7 @@
 // programadores donde cada equipo se encarga de una tarea específica. Usted forma parte del
 // equipo de programación que se encargará de hacer el módulo para los preventistas:
 
-// i) Desarrollar una interfaz por consola donde se solicite al usuario la cantidad de
-// clientes.
-// ii) Solicitar al usuario la carga de los clientes creados dinámicamente en el paso anterior.
-// iii) A medida que se dé de alta cada cliente, Generar aleatoriamente la cantidad de
-// productos asociados al cliente y sus características.
-// Ej: producto cargado nro. 2
-// Producto {
-// ProductoID=2
-// Cantidad = 1;
-// *TipoProducto = “Snack”;
-// PrecioUnitario = 100;
-// }
+
 // iv) Implemente una función que calcule el costo total de un producto. Esta función debe
 // recibir como parámetro el producto y devolver el resultado de calcular la Cantidad por
 // el PrecioUnitario.
@@ -56,13 +46,46 @@ int main()
     int cantidadClientes;
     printf("Ingrese la cantidad de clientes: ");
     scanf("%d", &cantidadClientes);
-    
+
     // Control de cantidad de clientes
-    if (cantidadClientes < 1 && cantidadClientes > MAX_CLIENTES) {
+    if (cantidadClientes < 1 && cantidadClientes > MAX_CLIENTES)
+    {
         printf("\nCantidad de clientes incorrecta, los clientes deben estar entre 1 y 5.");
         return 1;
     }
 
+    // Arreglo dinámico de clientes
+    struct Cliente *clientes = (struct Cliente *)malloc(sizeof(struct Cliente) * cantidadClientes);
+
+    // Carga de datos para clientes
+    for (int i = 0; i < cantidadClientes; i++)
+    {
+        clientes[i].ClienteID = i + 1;
+
+        // Nombre del cliente
+        char buffer[100];
+        printf("\nIngrese el nombre del cliente: ");
+        gets(buffer);
+        clientes[i].NombreCliente = (char *)malloc(strlen(buffer) + 1);
+        strcpy(clientes[i].NombreCliente, buffer); // strcpy(destino, origen)
+
+        // Cantidad aleatoria de productos
+        clientes[i].CantidadProductosAPedir = (rand() % 5) + 1; // Valores entre 1 y x, siempre (rand() % x) + 1
+
+        // Arreglo dinámico de productos
+        clientes[i].Productos = (struct Producto *)malloc(sizeof(struct Producto) * clientes[i].CantidadProductosAPedir);
+
+        for (int j = 0; i < clientes[i].CantidadProductosAPedir; j++)
+        {
+            clientes[i].Productos[j].ProductoID = j + 1;
+            clientes[i].Productos[j].Cantidad = (rand() % 10) + 1;
+            int tipoIndex = (rand() % 5) + 1;
+            clientes[i].Productos[j].TipoProducto = TiposProductos[tipoIndex];
+            clientes[i].Productos[j].PrecioUnitario = rand() % (100 - 10 + 1) + 10; // Valores especificos: rand() % (max - min + 1) + min
+
+        }
+        
+    }
 
     return 0;
 }
